473cff411e2f54f19f0a7a6572a29188f33cbdcb
Madeleine Huish <madeleinehuish@gmail.com>
2018-06-22 14:04:56 -0700

    few improvements

--- index.js
+++ index.js
@@ -9,18 +9,16 @@ const { day, hour, timeCurrent, dayCurrent } = require('./time');
 //pull in code from keypress module
 const keypress = require('./keypress');
 
-//create the url to call api
+//create the url to call api with given day
 const baseUrl = 'https://data.sfgov.org/resource/bbb8-hzi6.json';
 const query = `${baseUrl}?dayorder=${day}`;
 
-//calling api with async/await syntax by date then filtering by current time
-//see bottom of file for alternate way to call this function with older syntax
+//calling api with async/await syntax with query above then filtering by current time
 const fetchTrucks = async () => {
 	const res = await axios.get(query);
 	const truckArray = await res.data;
 
 	const truckFiltered = truckArray.filter(truck => {
-
 		const start24 = Number(truck.start24.substr(0,2));
 		const end24 = Number(truck.end24.substr(0,2));
 
@@ -32,12 +30,12 @@ const fetchTrucks = async () => {
 	const alphabetical = _.sortBy(truckFiltered, ['applicant']);
 
 	//print out list of trucks
-	console.log(`Trucks open in SF at ${timeCurrent} on ${dayCurrent}`, '\n');
+	console.log('\n','\x1b[32m',`Trucks open in SF at ${timeCurrent} on ${dayCurrent}`, '\n', '\x1b[0m');
 
 	for(let i = 0; i < alphabetical.length; i++) {
 
-		if (i%10 === 0 && i!==0) {
-			console.log('Press any key for more or ctrl-C to exit');
+		if (i%10 === 0 && i!==0) {  //only 10 at a time
+			console.log('\x1b[32m','Press any key for more or ctrl-C to exit','\x1b[0m', '\n');
 			await keypress();
 		}
 		console.log(alphabetical[i].applicant, '\n', alphabetical[i].locationdesc, '\n');
--- time.js
+++ time.js
@@ -2,7 +2,7 @@
 const date = new Date();
 const day = date.getDay();
 const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-let hour = date.getHours();
+const hour = date.getHours();
 let minutes = date.getMinutes();
 if (minutes < 10) {
   minutes = '0' + minutes;
